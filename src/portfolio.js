3b68f84d468c074955fd375a8231a43c4ed32c87
Muhammad Arif Rahmad <111945507+mhdarif09@users.noreply.github.com>
2023-02-07 19:14:54 -0800

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -316,16 +316,16 @@ const blogSection = {
   displayMediumBlogs: "true", // Set true to display fetched medium blogs instead of hardcoded ones
   blogs: [
     {
-      url: "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
-      title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
+      url: "https://bangsin.id/category/bug/information-disclosure/",
+      title: "Mengenal Bug Information Disclosure",
       description:
-        "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?"
+        "Information Disclosure, juga dikenal sebagai kebocoran informasi, adalah ketika sebuah situs web secara tidak sengaja mengungkapkan informasi sensitif kepada penggunanya"
     },
     {
-      url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
-      title: "Why REACT is The Best?",
+      url: "https://bangsin.id/mengenal-percabangan-di-python/",
+      title: "Mengenal Percabangan di Python",
       description:
-        "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
+        "Percabangan if adalah struktur kontrol yang memungkinkan program untuk membuat keputusan berdasarkan kondisi tertentu. Jika kondisi bernilai benar, program akan mengeksekusi satu atau lebih pernyataan."
     }
   ],
   display: true // Set false to hide this section, defaults to true
@@ -343,8 +343,8 @@ const talkSection = {
     {
       title: "Build Actions For Google Assistant",
       subtitle: "Codelab at GDG DevFest Karachi 2019",
-      slides_url: "https://bit.ly/saadpasta-slides",
-      event_url: "https://www.facebook.com/events/2339906106275053/"
+      slides_url: "#",
+      event_url: "#"
     }
   ],
   display: true // Set false to hide this section, defaults to true
